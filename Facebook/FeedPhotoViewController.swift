412a48dac7dba92f499d1c6ded8f066ea79199c0
Lauren Tsung <ltsung@yahoo-inc.com>
2014-09-29 19:36:10 -0700

    Made a patch for background color translucency

--- Facebook/FeedPhotoViewController.swift
+++ Facebook/FeedPhotoViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class FeedPhotoViewController: UIViewController {
+class FeedPhotoViewController: UIViewController, UIScrollViewDelegate {
 
     var image: UIImage!
     
+    @IBOutlet var theView: UIView!
     
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var doneButton: UIButton!
+    
+    @IBOutlet weak var actionButtons: UIImageView!
+    
+    var alphaCount: CGFloat!
+    
+    var dismissedModal: Bool!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         imageView.image = image
+        scrollView.contentSize = imageView.image!.size
+        scrollView.delegate = self
+        
+        alphaCount = 1.0
+        
+        dismissedModal = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,18 +44,66 @@ class FeedPhotoViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
     @IBAction func dismissViewController(sender: AnyObject) {
+        println("dismiss")
+        doneButton.alpha = 0
+        actionButtons.alpha = 0
+        theView.backgroundColor = UIColor(white: 0, alpha: 0)
+        
         dismissViewControllerAnimated(true, completion: nil)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func scrollViewDidScroll(scrollView: UIScrollView!) {
+        
+        
+        alphaCount = alphaCount - 0.05
+        
+        theView.backgroundColor = UIColor(white: 0, alpha: alphaCount)
+        
+        
     }
-    */
 
+    func scrollViewWillBeginDragging(scrollView: UIScrollView!) {
+        println("fade out the buttons")
+        
+        
+        doneButton.alpha = 0
+        actionButtons.alpha = 0
+    }
+
+    func scrollViewDidEndDragging(scrollView: UIScrollView!,
+        willDecelerate decelerate: Bool) {
+            
+      
+        println("content offset \(scrollView.contentOffset.y)")
+        
+        if (scrollView.contentOffset.y < -100) {
+            println("Dismiss me")
+              alphaCount = 0
+            
+            dismissViewControllerAnimated(true, completion: nil)
+            dismissedModal = true
+        }
+        else {
+            println("Not enough for treshold")
+            doneButton.alpha = 1
+            actionButtons.alpha = 1
+            
+            alphaCount = 1.0
+        }
+
+    
+    }
+
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView!) {
+        
+        if (!dismissedModal) {
+            theView.backgroundColor = UIColor(white: 0, alpha: 1)
+        }
+        else {
+            theView.backgroundColor = UIColor(white: 0, alpha: 0)
+        }
+    }
+   
 }
--- Facebook/NewsFeedViewController.swift
+++ Facebook/NewsFeedViewController.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class NewsFeedViewController: UIViewController {
+class NewsFeedViewController: UIViewController, UIViewControllerTransitioningDelegate, UIViewControllerAnimatedTransitioning {
     
     @IBOutlet weak var scrollView: UIScrollView!
-    
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var examplePhoto: UIImageView!
+    var isPresenting: Bool = true
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,20 +35,107 @@ class NewsFeedViewController: UIViewController {
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        var destinationViewController = segue.destinationViewController as FeedPhotoViewController
         
-        destinationViewController.image = self.examplePhoto.image
         
+        if (segue.identifier == "photoView") {
+            var destinationViewController = segue.destinationViewController as FeedPhotoViewController
+            
+            
+            destinationViewController.modalPresentationStyle = UIModalPresentationStyle.Custom
+            destinationViewController.transitioningDelegate = self
+            
+            destinationViewController.image = self.examplePhoto.image
+
+        }
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func animationControllerForPresentedController(presented: UIViewController!, presentingController presenting: UIViewController!, sourceController source: UIViewController!) -> UIViewControllerAnimatedTransitioning! {
+        isPresenting = true
+        return self
     }
-    */
+    
+    func animationControllerForDismissedController(dismissed: UIViewController!) -> UIViewControllerAnimatedTransitioning! {
+        isPresenting = false
+        return self
+    }
+    
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
+        // The value here should be the duration of the animations scheduled in the animationTransition method
+        return 0.5
+    }
+    
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        println("animating transition")
+        var containerView = transitionContext.containerView()
+        var toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
+        var fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
+        
+        var tempImageView = UIImageView(image: self.examplePhoto.image)
+        if (isPresenting) {
+            containerView.addSubview(toViewController.view)
+            toViewController.view.alpha = 0
+            
+            var window = UIApplication.sharedApplication().keyWindow
+    
+            
+        
+            tempImageView.contentMode = UIViewContentMode.ScaleAspectFill
+            
+            tempImageView.frame.size = CGSizeMake(154.0, 157.0)
+            
+            tempImageView.frame.origin.x = 4
+            tempImageView.frame.origin.y = 190
+            tempImageView.clipsToBounds = true
+            
 
+            
+            window.addSubview(tempImageView)
+            self.examplePhoto.hidden = true
+       
+            
+           
+            
+            
+            UIView.animateWithDuration(0.5, animations: { () -> Void in
+               
+                toViewController.view.alpha = 1
+                
+                tempImageView.frame.origin.x += 75
+                tempImageView.frame.origin.y += 20
+
+                tempImageView.clipsToBounds = false
+                
+                tempImageView.transform = CGAffineTransformMakeScale(2, 2)
+
+
+                }) { (finished: Bool) -> Void in
+                    tempImageView.removeFromSuperview()
+                    transitionContext.completeTransition(true)
+            }
+
+           
+
+            
+        } else {
+  
+            self.examplePhoto.hidden = false
+
+            
+            UIView.animateWithDuration(0.6, animations: { () -> Void in
+                println("Dismissing feed photo view controller")
+                
+                //fromViewController.view.alpha = 0
+                
+                containerView.backgroundColor = UIColor(white: 0, alpha: 0)
+                
+                containerView.frame.size = CGSizeMake(154.0, 230.0)
+                containerView.frame.origin.x = 4
+                containerView.frame.origin.y = 145
+                
+                }) { (finished: Bool) -> Void in
+                    transitionContext.completeTransition(true)
+                    fromViewController.view.removeFromSuperview()
+            }
+        }
+    }
 }
